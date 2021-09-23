62179c0b5e48ce64ae6646f70e4755673c33f830
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-09-23 21:14:55 +0200

    Ok

--- index.js
+++ index.js
@@ -104,6 +104,90 @@
 
 
 
+
+
+
+
+
+
+
+
+// ONLY for DANISH KEYBOARD
+ // ===================== Allow - Only Numbers,Comma,+,*,-,%,:,/ ===============================================================
+ const  onlyNumbersAndSomeChars = (event) => 
+ {
+      var keycode = (event.which) ? event.which : event.keyCode;
+  
+      if(event.shiftKey && (keycode > 34 && keycode < 44)) // Disable SHIFT
+      {
+         return true;
+      } 
+      else if(event.shiftKey && (keycode == 191 || keycode == 48 || keycode == 53 || keycode == 56 || keycode == 57 || keycode == 55) ) // Special chars %/()=+
+      {
+          return true;
+      }
+      else if(keycode == 189) // -
+      {
+          return true;
+      }
+      else if(keycode == 187) // +
+      {
+          return true;
+      }
+      else if(event.shiftKey)
+      {
+          return false;
+      }
+      else if(event.ctrlKey && event.altKey) // Disable CTRL + ALT
+      {
+         return false;
+      }
+      else if (event.ctrlKey && keycode == 65) // CTRL + A
+      {
+       return true;
+      }
+      else if(event.ctrlKey && keycode == 67) // CTRL + C
+      {
+        return true;
+      }
+      else if(event.ctrlKey && keycode == 86) // CTRL + V
+      {
+        return true;
+      }
+      else if(keycode === 188 || keycode === 190) // Comma
+      {
+        return true;
+      }
+      else if(keycode > 112) // All others not numbers
+      {
+       return false;
+       //  event.preventDefault();
+      }
+      else if ((keycode > 7 && keycode < 16) || (keycode > 34 && keycode < 65) || (keycode > 92 && keycode < 112)) // 8 = backspace & 46 = DEL  || 48-57 = numbers
+      {
+          console.log(keycode);
+          return true;
+      } 
+      else
+       {
+          return false;
+      }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
      
           // ===================== Allow - Only Letters ===============================================================
           
