763a2b4fc6cb738633c4aa35e48422d38b28e2f4
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-09-23 19:57:08 +0200

    Not ok

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
      {
           var keycode = (event.which) ? event.which : event.keyCode;
       
-          if(event.shiftKey)
+          if(event.shiftKey) // Disable SHIFT
           {
              return false;
           }
-          else if (event.ctrlKey && keycode == 65)
+          if(event.ctrlKey && event.altKey) // Disable CTRL + ALT
+          {
+             return false;
+          }
+          else if (event.ctrlKey && keycode == 65) // CTRL + A
           {
            return true;
           }
-          else if ((keycode == 8 || keycode == 46) || (keycode > 45 && keycode < 65)) // 8 = backspace & 46 = DEL  || 48-57 = numbers
+          else if(event.ctrlKey && keycode == 67) // CTRL + C
+          {
+            return true;
+          }
+          else if(event.ctrlKey && keycode == 86) // CTRL + V
+          {
+            return true;
+          }
+          else if ((keycode > 8 && keycode < 16) || (keycode > 34 && keycode < 65) || (keycode > 92 && keycode < 106)) // 8 = backspace & 46 = DEL  || 48-57 = numbers
           {
               console.log(keycode);
               return true;
           }
           else
            {
+              console.log("FALSE:" + keycode);
               return false;
           }
      }
